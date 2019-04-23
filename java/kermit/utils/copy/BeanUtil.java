5a45af9633c918416715a232fe82175f19c2d662
sunboyu <sunboyu@huashenghaoche.com>
2019-04-23 14:34:28 +0800

    更新工具类

---
+++ java/kermit/utils/copy/BeanUtil.java
@@ -0,0 +1,26 @@
+package kermit.utils.copy;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.converters.*;
+
+/**
+ * @Date: 2019/4/2314:17
+ * @Author: BoyuSun
+ * @Description: BeanUtils扩展
+ * org.apache.commons.beanutils.BeanUtils的
+ */
+public class BeanUtil extends BeanUtils {
+    private BeanUtil() {}
+
+    static {
+        //注册sql.date的转换器，即允许BeanUtils.copyProperties时的源目标的sql类型的值允许为空
+        ConvertUtils.register(new SqlDateConverter(null), java.sql.Date.class);
+        ConvertUtils.register(new SqlDateConverter(null), java.util.Date.class);
+        ConvertUtils.register(new SqlTimestampConverter(null), java.sql.Timestamp.class);
+        ConvertUtils.register(new IntegerConverter(null), Integer.class);
+        ConvertUtils.register(new DoubleConverter(null), Double.class);
+        ConvertUtils.register(new FloatConverter(null), Float.class);
+        ConvertUtils.register(new LongConverter(null), Long.class);
+    }
+}
---
+++ java/kermit/utils/verify/AssertUtil.java
@@ -0,0 +1,12 @@
+package kermit.utils.verify;
+
+import com.sun.deploy.util.StringUtils;
+
+/**
+ * @Date: 2019/4/2314:18
+ * @Author: BoyuSun
+ * @Description: 判断
+ */
+public class AssertUtil {
+
+}
